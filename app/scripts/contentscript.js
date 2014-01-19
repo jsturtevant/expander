1908971ef6263c6448a4aca6a52fa92cd0928c2b
jsturtevant <jsturtevant@gmail.com>
2014-01-19 11:46:12 -0500

    added changable short cut key

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,16 +2,28 @@
 /**
  * Created by jsturtevant on 1/18/14.
  */
-var key = "";
-var value = "";
+var options = {};
 
 chrome.runtime.sendMessage({method: "getOptions"}, function(response) {
-    key = response.options.shortcuts[0].key;
-    value = response.options.shortcuts[0].value;
+    options = response.options
+
+    Mousetrap.bind(options.shortcutKey, function(e) {
+        var elementList = document.querySelectorAll('input[type=text]');
+        var textboxes = Array.prototype.slice.call(elementList,0);
+        textboxes.forEach(function(textbox){
+            var string = textbox.value;
+
+            if (string.match(options.shortcuts[0].key)){
+                var newValue = string.replace(options.shortcuts[0].key,options.shortcuts[0].value);
+                textbox.value = newValue;
+            }
+        });
+
+        return false;
+    });
 });
 
 Mousetrap.stopCallback =function(e, element, combo) {
-
     // if the element has the class "mousetrap" then no need to stop
     if ((' ' + element.className + ' ').indexOf(' mousetrap ') > -1) {
         return false;
@@ -21,19 +33,3 @@ Mousetrap.stopCallback =function(e, element, combo) {
     return  element.tagName == 'SELECT' || element.tagName == 'TEXTAREA' || (element.contentEditable && element.contentEditable == 'true');
 }
 
-Mousetrap.bind('ctrl+shift+k', function(e) {
-    //document.body.style.backgroundColor="red";
-
-    var elementList = document.querySelectorAll('input[type=text]');
-    var textboxes = Array.prototype.slice.call(elementList,0);
-    textboxes.forEach(function(textbox){
-        var string = textbox.value;
-
-        if (string.match(key)){
-            var newValue = string.replace(key, value);
-            textbox.value = newValue;
-        }
-    });
-
-    return false;
-});
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,16 +1,18 @@
 /**
  * Created by jsturtevant on 1/18/14.
  */
-
+var DEFAULT_SHORTCUT_KEY = 'ctrl+shift+k';
 
 // Saves options to localStorage.
 function save_options() {
     var keyElement = document.getElementById("expandKey");
     var valueElement = document.getElementById("expandValue");
+    var shortcutElement = document.getElementById("shortcut");
 
     var options = {};
     options.shortcuts =[];
     options.shortcuts.push({'key': keyElement.value, 'value': valueElement.value});
+    options.shortcutKey = shortcutElement.value || DEFAULT_SHORTCUT_KEY;
     localStorage["options"] = JSON.stringify(options);
 
     // Update status to let user know options were saved.
@@ -24,13 +26,22 @@ function save_options() {
 // Restores select box state to saved value from localStorage.
 function restore_options() {
     var optionsJSON = localStorage["options"];
-    var options = JSON.parse(optionsJSON);
+    var options ={};
+    if (!optionsJSON){
+        options.shortcuts =[];
+        options.shortcuts.push({'key': '', 'value': ''});
+    }
+    else{
+       options = JSON.parse(optionsJSON);
+    }
 
     var keyElement = document.getElementById("expandKey");
     var valueElement = document.getElementById("expandValue");
+    var shortcutElement = document.getElementById("shortcut");
 
     keyElement.value = options.shortcuts[0].key;
     valueElement.value = options.shortcuts[0].value;
+    shortcutElement.value = options.shortcutKey || DEFAULT_SHORTCUT_KEY;
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
