d2be31c5f93fa0a8fecc3e318ce5c213e8030718
jsturtevant <jsturtevant@gmail.com>
2014-01-24 07:55:53 -0500

    swapped out manual options with ember.  Can add multple items now

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -3,45 +3,32 @@
  */
 var DEFAULT_SHORTCUT_KEY = 'ctrl+shift+k';
 
-// Saves options to localStorage.
-function save_options() {
-    var keyElement = document.getElementById("expandKey");
-    var valueElement = document.getElementById("expandValue");
-    var shortcutElement = document.getElementById("shortcut");
+var defaultOptions = {
+    shortcuts: [
+        {key: "", value:""}
+    ],
+    shortcutKey: DEFAULT_SHORTCUT_KEY
+};
 
-    var options = {};
-    options.shortcuts =[];
-    options.shortcuts.push({'key': keyElement.value, 'value': valueElement.value});
-    options.shortcutKey = shortcutElement.value || DEFAULT_SHORTCUT_KEY;
-    localStorage["options"] = JSON.stringify(options);
-
-    // Update status to let user know options were saved.
-    var status = document.getElementById("status");
-    status.innerHTML = "Options Saved.";
-    setTimeout(function() {
-        status.innerHTML = "";
-    }, 750);
-}
-
-// Restores select box state to saved value from localStorage.
-function restore_options() {
-    var optionsJSON = localStorage["options"];
-    var options ={};
+var myApp = angular.module('myApp', []);
+myApp.factory('Data', function() {
+    var optionsJSON = localStorage["optionsstore"];
     if (!optionsJSON){
-        options.shortcuts =[];
-        options.shortcuts.push({'key': '', 'value': ''});
+        return defaultOptions;
     }
     else{
-       options = JSON.parse(optionsJSON);
+        return JSON.parse(optionsJSON);
     }
+})
 
-    var keyElement = document.getElementById("expandKey");
-    var valueElement = document.getElementById("expandValue");
-    var shortcutElement = document.getElementById("shortcut");
+function OptionsCtrl($scope, Data){
+    $scope.options = Data;
 
-    keyElement.value = options.shortcuts[0].key;
-    valueElement.value = options.shortcuts[0].value;
-    shortcutElement.value = options.shortcutKey || DEFAULT_SHORTCUT_KEY;
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.querySelector('#save').addEventListener('click', save_options);
+    $scope.AddShortcut = function(){
+      $scope.options.shortcuts.push({key: "", value:""})
+    };
+
+    $scope.Save = function(){
+        localStorage["optionsstore"] = JSON.stringify($scope.options);
+    };
+};
