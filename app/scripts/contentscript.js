7fb1940956c01e17795a9c1f1b15014f7ef3c179
jsturtevant <jsturtevant@gmail.com>
2014-01-28 21:27:45 -0500

    created more descriptive functions

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -8,19 +8,27 @@ chrome.runtime.sendMessage({method: "getOptions"}, function(response) {
     options = response.options;
 
     Mousetrap.bind(options.shortcutKey, function(e) {
-        var elementList = document.querySelectorAll('input[type=text], textarea');
-        var textboxes = Array.prototype.slice.call(elementList,0);
-        textboxes.forEach(function(textbox){
-            var string = textbox.value;
-
-            if (string.match(options.shortcuts[0].key)){
-                textbox.value = string.replace(options.shortcuts[0].key, options.shortcuts[0].value);
-            }
-        });
-
+        var textboxes = SelectElements();
+        ExpandAllOccurrances(textboxes);
         return false;
     });
 });
+
+function ExpandAllOccurrances(elements) {
+    elements.forEach(function (textbox) {
+        var string = textbox.value;
+
+        if (string.match(options.shortcuts[0].key)) {
+            textbox.value = string.replace(options.shortcuts[0].key, options.shortcuts[0].value);
+        }
+    });
+}
+
+function SelectElements() {
+    var elementList = document.querySelectorAll('input[type=text], textarea');
+    var textboxes = Array.prototype.slice.call(elementList, 0);
+    return textboxes;
+}
 
 Mousetrap.stopCallback =function(e, element, combo) {
     // if the element has the class "mousetrap" then no need to stop
