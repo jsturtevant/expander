8aa86234e4e48c25d2a31c3ca6612444874d2654
jsturtevant <jsturtevant@gmail.com>
2014-02-07 09:17:36 -0500

    added advance option for css selectors

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -8,6 +8,7 @@ var defaultOptions = {
     shortcuts: [
         {key: "", value:""}
     ],
+    cssSelectors: [{value:""}],
     shortcutKey: DEFAULT_SHORTCUT_KEY
 };
 
@@ -31,11 +32,19 @@ function OptionsCtrl($scope, OptionsData){
       $scope.options.shortcuts.push({key: "", value:""})
     };
 
+    $scope.AddSelector = function(){
+        $scope.options.cssSelectors.push({value: ""})
+    };
+
     $scope.Save = function(){
         localStorage[LOCAL_STORAGE_KEY] = JSON.stringify($scope.options);
     };
 
     $scope.Delete = function (index){
         $scope.options.shortcuts.splice(index,1);
+    };
+
+    $scope.DeleteSelector = function (index){
+        $scope.options.cssSelectors.splice(index,1);
     };
 };
